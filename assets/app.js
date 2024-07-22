9ffbb7920a4d5c4aaafe1b44d9534090bad7eb84
Spirit-Souls <153604682+Spirit-Souls@users.noreply.github.com>
2024-07-22 14:32:25 +0200

    22/07/2024(reset)

--- assets/app.js
+++ assets/app.js
@@ -8,3 +8,25 @@ import './bootstrap.js';
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+const images = [
+    "/img/gececamisi.webp",
+    "/img/daymosque.webp", // Ajoutez les chemins vers vos autres images ici
+    "/img/sunsetmosque.webp",
+    "/img/germanmosque.webp",
+    "/img/famousmosque.webp"
+];
+
+let currentIndex = 0;
+const backgroundElement = document.getElementById('background');
+
+function changeBackground() {
+    backgroundElement.classList.add('fade-out');
+    setTimeout(() => {
+        currentIndex = (currentIndex + 1) % images.length;
+        backgroundElement.style.backgroundImage = `url(${images[currentIndex]})`;
+        backgroundElement.classList.remove('fade-out');
+    }, 1000); // La durée du fondu doit correspondre à celle définie dans le CSS
+}
+
+setInterval(changeBackground, 5000); // Change l'image toutes les 5 secondes
